65990dae2a9e542556796bfd7c17520c672ee35c
Bernard Strong <bstrong71@gmail.com>
2017-10-12 14:17:55 -0400

    initial commit

---
+++ src/main/java/com/example/hrPocBack/HrPocBackApplication.java
@@ -0,0 +1,12 @@
+package com.example.hrPocBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPocBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPocBackApplication.class, args);
+	}
+}
